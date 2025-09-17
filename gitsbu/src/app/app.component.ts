7bd7ca941d0699fc61fe7af78c4934df07203c0c
itsjustgui <itsjustgui@gmail.com>
2025-09-17 04:47:51 +0100

    added loadingIntro video

--- gitsbu/src/app/app.component.ts
+++ gitsbu/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'gitsbu';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  showSplash = true;
+  @ViewChild('title') title!: ElementRef;
+  @ViewChild('subtitle') subtitle!: ElementRef;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      gsap.from(this.title.nativeElement, { duration: 3, y: -50, opacity: 100, ease: 'power2.out' });
+      gsap.from(this.subtitle.nativeElement, { duration: 1, y: 50, opacity: 0, ease: 'power2.out', delay: 0.5 });
+    }, 3100);
+  }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.showSplash = false;
+    }, 5000);
+  }
 }
